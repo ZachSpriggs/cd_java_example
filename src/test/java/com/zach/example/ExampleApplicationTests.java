4353e21773682d1244f2ab9beef08345f24d9dad
Zach Spriggs <zachspriggs@gmail.com>
2024-12-03 18:42:22 -0800

    pushing Example for java stack at coding dojo

---
+++ src/test/java/com/zach/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zach.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
